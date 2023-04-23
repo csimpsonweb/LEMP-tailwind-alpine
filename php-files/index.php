fc2ad20f27ba2f1d0b6da0bd485c40b1ec42b146
Carl Simpson <carl.simpson@gmail.com>
2023-04-23 11:48:55 +0100

    add add files

---
+++ php-files/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Smashing HTML5!</title>
+
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+
+    <!--[if IE]>
+    <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lte IE 7]>
+    <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body id="index" class="home">
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+</body>
+</html>
